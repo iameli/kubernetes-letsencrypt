a3d70ef8a3f0da6151ad6ffd97d60ce539a66fde
Eli Mallon <eli@iame.li>
2016-01-21 13:28:25 -0800

    Update run.sh
    So people don't accidentally request a bunch of certs for my domain.

--- example/run.sh
+++ example/run.sh
@@ -3,8 +3,7 @@
 # Example script to generate your certs automatically.
 podName=$(kubectl get pods -l 'app=letsencrypt' -o name | sed 's/pod\///')
 kubectl exec -it "$podName" /app/get-cert.sh \
-  dandiprat.industries \
-  www.dandiprat.industries \
-  api.dandiprat.industries \
-  jenkins.dandiprat.industries
+  example.com \
+  www.example.com \
+  api.example.com
 
